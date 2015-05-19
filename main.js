6c4b5eb03c56643818c67a79a67d499ca157cdde
Kyle Richardson <s.k.richardson7@gmail.com>
2015-05-18 21:24:08 -0400

    numbers and operators work and display, but calculations git NaN

--- main.js
+++ main.js
@@ -1,18 +1,63 @@
+
 (function(){
   "use strict";
 
-})();
+
 
 var calculation = 0;
 var displayCalculation = '0';
 var $display = document.querySelector('.display');
+var pendingOperation;
+var buttonPressedLast;
+var input;
 
-//function for number press
+// function for number press
 function numberPress(event){
   var number=Number(event.target.textContent);
-  alert(numberPressed);
+  $display.textContent = number;
+  var input=number;
+  console.log(input);
 }
 
-[].forEach.call(document.querySelectorAll('.keypad-button.number'), function(el){
-  el.addEventListener('click', numberPress);
+
+//switch statement for operators
+function operatorPress(event){
+  var operator=event.target.textContent;
+
+    switch (operator) {
+      case "+":
+        calculation += input;
+        break;
+      case "-":
+        calculation -= input;
+        break;
+      case "*":
+        calculation *= input;
+        break;
+      case "/":
+        calculation /= input;
+        break;
+      }
+      console.log("operator clicked is"+ operator);
+}
+
+
+function equalsPressed(event){
+  var button= event.target.textContent;
+  var text=button.textContent;
+  $display.textContent = calculation;
+  console.log("calc is" + calculation);
+}
+
+[].forEach.call(document.querySelectorAll('.number'), function(element){
+  element.addEventListener('click', numberPress);
 },false);
+
+[].forEach.call(document.querySelectorAll('.operator'), function(element){
+  element.addEventListener('click', operatorPress);
+},false);
+
+document.querySelector('.equals').addEventListener('click', equalsPressed)
+
+
+})();
